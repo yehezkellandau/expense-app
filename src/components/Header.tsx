cf4e37df851424b692ad3bbed85b51e98ee01ecb
yeheland <yeland_97@yhaoo.fr>
2025-04-07 13:01:50 -0400

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="text-xl font-medium border-b-1 border-b border-gray-300 py-5">
+            <h1>Expenses App</h1>
+        </header>
+    );
+};
+export default Header;
